d52ee28745e5ed49815a0279d312128a7a20fbb9
Emma Waltho <ejwaltho@gmail.com>
2023-01-10 18:45:10 -0800

    javascript 20% done, css touchups

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,25 @@
-var goBtn = document.querySelector('.goBtn')
-
-var cityName = document.querySelector('.input').value
-var APIKey = '57cf35bb4ba11b5c26dba9f423cf261f'
-var requestUrl = 'api.openweathermap.org/data/2.5/forecast?q={cityName}&appid={API key}' 
-var cityText = document.getElementById('#city')
+var goBtn = document.querySelector('.goBtn');
+var requestUrl = 'https://api.github.com/users/ewaltho/repos' 
 
 
-function getApi(){ 
+goBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    var hiddenContent = document.getElementsByClassName('results');
+    var cityNameSearch = document.getElementById('input').value
+    //add line to clear search value to be empty upon submit
+    var cityText = document.getElementById('city')
+    var APIKey = '57cf35bb4ba11b5c26dba9f423cf261f'
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityNameSearch + '&appid=' + APIKey 
+    var hiddenEl = document.getElementById('hidden')
+    cityText.textContent = cityNameSearch;
+    hiddenEl.classList.remove('hide');
     fetch(requestUrl)
         .then(function (response) {
-            return(response).json();
+            return response.json();
         })
         .then(function (data) {
             console.log(data)
         })
-    }
+
+
+})
